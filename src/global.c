faf0c4afb9c7c32898a10c397d2e015da675d9e7
wdd00 <572718113@qq.com>
2017-01-18 16:51:26 +0800

    extract parameters

--- src/global.c
+++ src/global.c
@@ -2,38 +2,47 @@
 #include "smr-simulator/smr-simulator.h"
 #include "main.h"
 
-int BandOrBlock = 1;
-/* Block = 0,Band =1*/
 
-unsigned long NSSDBuffers = 3560*5;
-unsigned long NSSDBufTables = 3560*5;
-unsigned long SSD_BUFFER_SIZE = 4096;
+unsigned long NSSDBuffers;
+unsigned long NSSDBufTables;
+/*unsigned long NSSDBuffers = 1000;
+unsigned long NSSDBufTables = 500;*/
+unsigned long SSD_BUFFER_SIZE;
 unsigned long NSMRBands = 194180;		// 194180*(18MB+36MB)/2~5TB
 unsigned long NSMRBlocks = 2621952;		// 2621952*8KB~20GB
-unsigned long NSSDs = 3560;
-unsigned long NSSDTables = 3560;
-unsigned long NBANDTables = 2621952;
-size_t SSD_SIZE = 4096;
-size_t BLCKSZ = 4096;
-size_t BNDSZ = 36*1024*1024;
-size_t ZONESZ = 2*1024*1024;
-unsigned long INTERVALTIMELIMIT = 100000000;
-unsigned long NSSDLIMIT = 3560;
+//unsigned long NSSDs = 2621952;		// 2621952*8KB~20GB
+unsigned long NSSDs;
+unsigned long NSSDTables;
+//unsigned long NSSDLIMIT = 500000;
+//unsigned long NSSDCLEAN = 20000;
+unsigned long NSSDLIMIT;
 unsigned long NSSDCLEAN = 1;
 unsigned long WRITEAMPLIFICATION = 100;
-unsigned long NCOLDBAND = 1;
-unsigned long PERIODTIMES = 3560;
+unsigned long NCOLDBAND = 100;
+unsigned long PERIODTIMES;
+//unsigned long PERIODTIMES = 10000000;
+//unsigned long NSSDLIMIT = 2500000;
+//unsigned long NSSDCLEAN = 100000;
+/*unsigned long INTERVALTIMELIMIT = 1000;
+unsigned long NSSDLIMIT = 400;
+unsigned long NSSDCLEAN = 200;*/
+//char smr_device[100] = "/dev/sdc2";
+//char ssd_device[100] = "/dev/sda1";
+//char inner_ssd_device[100] = "/dev/hioa2";
+//char smr_device[] = "/github/smr-ssd-cache/smr";
+//char ssd_device[] = "/github/smr-ssd-cache/ssd";
+//char inner_ssd_device[] = "/github/smr-ssd-cache/inner_ssd";
 char smr_device[] = "/Users/wangchunling/Software/code/smr-test/smr-ssd-cache/src/smr";
 char ssd_device[] = "/Users/wangchunling/Software/code/smr-test/smr-ssd-cache/src/ssd";
 char inner_ssd_device[] = "/Users/wangchunling/Software/code/smr-test/smr-ssd-cache/src/inner_ssd";
 //SSDEvictionStrategy EvictStrategy = CLOCK;
 //SSDEvictionStrategy EvictStrategy = LRUofBand;
-SSDEvictionStrategy EvictStrategy = Most;
+SSDEvictionStrategy EvictStrategy;
 //SSDEvictionStrategy EvictStrategy = LRU;
 //SSDEvictionStrategy EvictStrategy = SCAN;
 //SSDEvictionStrategy EvictStrategy = WA;
-SSDEvictionStrategy EvictStrategy = MaxCold;
-//int BandOrBlock = 0;
+//SSDEvictionStrategy EvictStrategy = MaxCold;
+int BandOrBlock = 0;
 /*Block = 0, Band=1*/
 int 		    smr_fd;
 int 		    ssd_fd;
